e3d7859d9f4c8a0e69f84a5444ea6404402889f0
Gabriel marques do nascimento <gm2042225@gmail.com>
2025-04-11 07:10:36 -0300

    update

---
+++ cliente/v6/renderer.js
@@ -0,0 +1,22 @@
+
+
+
+export default function renderScreen(screen, game, requestAnimationFrame) {
+  ctx.clearRect(0, 0, 10, 10);
+
+  for (const playerId in game.state.players) {
+    const player = game.state.players[playerId];
+    ctx.fillStyle = "black";
+    ctx.fillRect(player.x, player.y, 1, 1);
+  }
+
+  for (const fruitId in game.state.fruits) {
+    const fruit = game.state.fruits[fruitId];
+    ctx.fillStyle = "green";
+    ctx.fillRect(fruit.x, fruit.y, 1, 1);
+  }
+
+  requestAnimationFrame(()=> {
+    renderScreen(screen, game, requestAnimationFrame);
+  });
+}
